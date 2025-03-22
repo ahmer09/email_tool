9ed6e3a5af72324647f36d67f900e9995bf049f9
ahmer09 <ahmeriq09@hotmail.com>
2025-03-22 20:32:02 +0530

    remove init from main

--- email_tool/email_tool.py
+++ email_tool/email_tool.py
@@ -5,7 +5,3 @@ def create_and_show_email(to: str, subject: str, body: str):
     mailto_url = f"mailto:{to}?subject={subject}&body={body}"
     webbrowser.open(mailto_url, new=1)
     return None
-
-
-if __name__ == '__main__':
-    create_and_show_email('4u.ahmer@gmail.com', 'test', 'test')
