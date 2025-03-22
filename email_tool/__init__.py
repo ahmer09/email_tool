5482df843a53f8be7b10a46b771ea2c16029c2fd
ahmer09 <ahmeriq09@hotmail.com>
2025-03-22 20:36:03 +0530

    add init with package info

--- email_tool/__init__.py
+++ email_tool/__init__.py
@@ -1,3 +1,7 @@
-# __init__.py
+# emaiL_tool/__init__.py
 
-from email_tool.email_tool import *
+# Importing specific functions or classes from mymodule.py
+from .email_tool import create_and_show_email
+
+# You can also define module-level variables or functions here
+__version__ = "0.1"
--- email_tool/email_tool.py
+++ email_tool/email_tool.py
@@ -1,4 +1,4 @@
-#email_tool.py
+#email_tool/email_tool.py
 
 import webbrowser
 def create_and_show_email(to: str, subject: str, body: str):
